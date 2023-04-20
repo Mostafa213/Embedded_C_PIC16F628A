713a874db824669bd21e52b47ec11ffcdf03c12a
Mostafa213 <sasa7amo2232@gmail.com>
2023-04-20 17:48:10 +0200

    CCPcompar mode

---
+++ CCP_Comparing.X/main.c
@@ -0,0 +1,15 @@
+/*
+ * File:   main.c
+ * Author: Mostafa
+ *
+ * Created on April 18, 2023, 11:06 PM
+ */
+
+
+#include <xc.h>
+#include "config.h"
+#define _XTAL_FREQ 4000000
+
+void main(void) {
+    return;
+}
